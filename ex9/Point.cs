7ae040096183db879bc337018a64293f5908fa1d
murabye <vlada_kuz2011@mail.ru>
2017-06-29 19:30:07 +0500

    создание списка

---
+++ ex9/Point.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex9
+{
+    class Point
+    {
+        public Point Next { get; set; }
+        public string Info { get; set; }
+
+        public Point()
+        {
+            Info = "";
+            Next = null;
+        }
+        public Point(string info)
+        {
+            Info = info;
+        }
+    }
+}
--- ex9/Program.cs
+++ ex9/Program.cs
@@ -1,15 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MyLib;
 
 namespace ex9
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
+            // запрос длины ввода
+            var number = Ask.Num("Введите количество элементов списка: ", 1);
+
+            // создание первого элемента, инициализация текущего и предыдущего
+            var first = new Point(Ask.Word("Введите значение информационного поля (строка): "));
+            var cur = first;
+            var prev = first;
+
+            // создание списка
+            for (var i = 0; i < number; i++)
+            {
+                cur = new Point(Ask.Word("Введите значение информационного поля (строка): "));
+                prev.Next = cur;
+                prev = cur;
+            }
+
+
+
         }
     }
 }
